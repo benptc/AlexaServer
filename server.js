83508f8966fcad2d02777cc9cfdc823dfd7611f0
Ben Reynolds <breynolds@ptc.com>
2017-11-27 10:24:34 -0500

    updates via deploy script

--- server.js
+++ server.js
@@ -9,16 +9,4 @@ var server = new AlexaAppServer( {
 
 server.start();
 
-server.express.use('/test', function(req, res) { res.send(JSON.stringify(global.state)); });
-
-// var express = require('express')();
-var http = require('http').Server(server.express);
-var io = require('socket.io')(http);
-
-io.on('connection', function(socket){
-    console.log('a user connected');
-});
-//
-// http.listen(3000, function(){
-//     console.log('listening on *:3000');
-// });
+server.express.use('/getState', function(req, res) { res.send(JSON.stringify(global.state)); });
