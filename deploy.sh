5ca13529e04935543adcdaf01afedd3baf4cf07a
Ben Reynolds <breynolds@ptc.com>
2017-11-27 08:58:42 -0500

    updates via deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+CMD=$1
+
+function skill {
+    (cd /apps/RealityEditorSkill; git add .; git commit -m 'updates via deploy script'; git push origin master)
+} 
+
+function usage {
+    echo "Usage: $(basename $0) COMMAND
+
+  COMMANDS:
+
+    skill    commit and push the RealityEditorSkill submodule
+    server   commit and push the server and deploy to heroku
+";
+    exit 1;
+}
+
+case $CMD in
+    (skill) skill;;
+    (server) server;;
+    (*) usage;;
+esac
+
+exit 0;
